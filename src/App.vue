bcd3a490b2133d897ce8657b43b73057c2c436cd
郑方洁 <2040834929@qq.com>
2026-09-08 13:56:13 +0800

    Texture

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import Cube from './components/01-cube.vue'
 import Group from './components/02-group.vue'
 import WorldPosition from './components/03-worldPosition.vue'
 import MaterialGeometry from './components/04-MaterialGeometry.vue'
+import Texture from './components/05-texture.vue'
 </script>
 
 <template>
   <!-- <Cube />
   <Group /> -->
   <!-- <WorldPosition /> -->
-  <MaterialGeometry />
+  <!-- <MaterialGeometry /> -->
+  <Texture />
 </template>
